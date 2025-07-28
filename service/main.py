547c2a71d8f9c8fd6f01a3c1c5ea646f54c7a077
Karmpatel18 <karmpatel1203@gmail.com>
2025-07-28 21:31:01 +0530

    pinecone index added"

--- service/main.py
+++ service/main.py
@@ -11,6 +11,22 @@ app = FastAPI()
 # Initialize Pinecone with the API key from environment variables
 pc = Pinecone(api_key=os.getenv("PINE_API"))
 
+index_name = "semantic-search-index"
+
+if not pc.has_index(index_name):
+    pc.create_index_for_model(
+        name=index_name,
+        cloud="aws",
+        region="us-east-1",
+        embed={
+            "model":"llama-text-embed-v2",
+            "field_map":{"text": "chunk_text"}
+        }
+    )
+    
+    
+
+
 
 @app.get("/")
 def read_root():
