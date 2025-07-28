d7651d5b9e9bb22752fb95d6310fa7b30b0ff24c
Karmpatel18 <karmpatel1203@gmail.com>
2025-07-28 21:26:53 +0530

    pinecone initialize

--- service/main.py
+++ service/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pinecone import Pinecone
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
+
+# Initialize FastAPI application
 app = FastAPI()
+# Initialize Pinecone with the API key from environment variables
+pc = Pinecone(api_key=os.getenv("PINE_API"))
+
 
 @app.get("/")
 def read_root():
     return {'data': 'Hello, World!'}
+
+@app.post("/register")
+def register_user():
+    return {'data': 'User registered successfully!'}
